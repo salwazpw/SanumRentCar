17324b23f73e26c60eba069947e41f7ba27f8427
salwazpw <salwapw04@gmail.com>
2022-03-30 14:53:53 +0700

    Proyek 1 - Mengganti warna font akun

--- resources/views/web/navbar.blade.php
+++ resources/views/web/navbar.blade.php
@@ -14,7 +14,8 @@
           <li><a class="nav-link scrollto active" href="/contact">Contact</a></li>
           <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
+                                    <font color="F8BA36">{{ Auth::user()->name }}</font>
+                                    
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
