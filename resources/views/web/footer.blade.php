ce20d8b9a5c321bb9d6d605ba6c66876d360cf51
salwazpw <salwapw04@gmail.com>
2022-04-13 20:15:42 +0700

    Proyek 1 - Perubahan Footer

--- resources/views/web/footer.blade.php
+++ resources/views/web/footer.blade.php
@@ -16,8 +16,7 @@
                 <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
                 <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
                 <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-                <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-                <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+
               </div>
             </div>
           </div>
@@ -25,11 +24,10 @@
           <div class="col-lg-2 col-md-6 footer-links">
             <h4>Useful Links</h4>
             <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="/home">Home</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="/mobil">Mobil</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="/form">Form Rent</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="/contact">Contact</a></li>
             </ul>
           </div>
 
